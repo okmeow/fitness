d858a01198f51d1abb83caf9571df048fe3a446c
Ilia <ilytar@gmail.com>
2022-12-21 12:14:35 +0300

    Тест слайдера. Неправильно, доработать или удалить

--- source/js/modules/trainers.js
+++ source/js/modules/trainers.js
@@ -1,6 +1,3 @@
-import {swiper} from "vendor/swiper.js";
-
-
 const WidthHeightCardsConstants = {
   DESKTOP: 1.1307,
   TABLET: 1.097,
@@ -10,12 +7,46 @@ const WidthHeightCardsConstants = {
 const trainerCardElements = document.querySelectorAll('[data-trainer-card="data-trainer-card"]');
 const trainerCardExtraElements = document.querySelectorAll('[data-trainer-extra="data-trainer-extra"]');
 const trainersButtonNextElement = document.querySelector('[data-trainers-button-next="data-trainers-button-next"]');
-const trainersButtonPreviousElement = document.querySelector('[data-trainers-button-previous="data-trainers-button-previous"]');
+// const trainersButtonPreviousElement = document.querySelector('[data-trainers-button-previous="data-trainers-button-previous"]');
 
+const nextCardOn = () => {
+  for (let i = 0; i < trainerCardElements.length; i++) {
+    if (trainerCardElements[i].classList.contains('trainers__item--last-visible')) {
+      if (i === trainerCardElements.length - 1) {
+        trainerCardElements[4].style.display = 'none';
+        trainerCardElements[5].style.display = 'none';
+        trainerCardElements[6].style.display = 'none';
+        trainerCardElements[7].style.display = 'none';
+        trainerCardElements[0].style.display = 'block';
+        trainerCardElements[1].style.display = 'block';
+        trainerCardElements[2].style.display = 'block';
+        trainerCardElements[3].style.display = 'block';
 
+        trainerCardElements[7].classList.remove('trainers__item--last-visible');
+        trainerCardElements[3].classList.add('trainers__item--last-visible');
+        setCardsSize();
+        return;
+      }
+      if (i === 3) {
+        trainerCardElements[4].style.display = 'block';
+        trainerCardElements[5].style.display = 'block';
+        trainerCardElements[6].style.display = 'block';
+        trainerCardElements[7].style.display = 'block';
+        trainerCardElements[0].style.display = 'none';
+        trainerCardElements[1].style.display = 'none';
+        trainerCardElements[2].style.display = 'none';
+        trainerCardElements[3].style.display = 'none';
 
+        trainerCardElements[7].classList.add('trainers__item--last-visible');
+        trainerCardElements[3].classList.remove('trainers__item--last-visible');
+        setCardsSize();
+        return;
+      }
+    }
+  }
+};
 
-
+trainersButtonNextElement.addEventListener('click', nextCardOn);
 
 // Изменение размеров карточки от ширины экрана (с сохранением пропорций)
 
